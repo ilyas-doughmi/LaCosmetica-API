0eab972cbcdd07845faf3f1f8d42c7a1862d28a4
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-03-16 23:09:02 +0000

    feat: implement slug functionality in Product model with Spatie Sluggable

--- web/app/Models/Product.php
+++ web/app/Models/Product.php
@@ -3,8 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Product extends Model
 {
-    
+    use HasSlug;
+
+    protected $fillable = ['category_id','name','slug','description','price'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class)
+        ->withPivot('quantity','unit_price')
+        ->withTimestamps();
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+        ->generateSlugsFrom('name')
+        ->saveSlugsTo('slug');
+    }
 }
